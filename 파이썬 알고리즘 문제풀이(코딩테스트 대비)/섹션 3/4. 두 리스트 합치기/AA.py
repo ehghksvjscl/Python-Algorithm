ce6f4c410c28f28e7e7ca8be6166a89892ff02f4
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-30 13:14:19 +0900

    두 리스트 합치기

--- 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/4. 두 리스트 합치기/AA.py
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/4. 두 리스트 합치기/AA.py
@@ -1,10 +1,16 @@
 import sys
 import os
+from functools import reduce
 
 filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
 
 if sys.argv[-1] == "local":
     sys.stdin = open(filePath, "rt")
 
-print(input())
+firstListLength = input()
+firstList = list(map(int, input().split()))
+secondListLength = input()
+secondList = list(map(int, input().split()))
 
+result = firstList + secondList
+print(reduce(lambda t,x: str(t) + " " + str(x),sorted(result)))
