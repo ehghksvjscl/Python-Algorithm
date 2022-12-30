68862770ce555df1ce6429f1693cab981b2fc6a2
ehghksvjscl <ehghkswodnjs@naver.com>
2022-12-30 11:06:15 +0900

    refector: 경로, input 로직 변경

--- 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/3. 카드 역배치/AA.py
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/3. 카드 역배치/AA.py
@@ -2,8 +2,10 @@ import sys
 import os
 from functools import reduce
 
-filePath = "{}/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/3. 카드 역배치/in1.txt".format(os.getcwd())
-# sys.stdin = open(filePath, "rt")
+filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.argv[-1] == "local":
+    sys.stdin = open(filePath, "rt")
 
 cardList = [i for i in range(1, 21)]
 
---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/4. 두 리스트 합치기/AA.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.argv[-1] == "local":
+    sys.stdin = open(filePath, "rt")
+
+print(input())
+
