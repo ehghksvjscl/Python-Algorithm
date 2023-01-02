b99f7522e51e8fe4a962289af8bc0e7211d5cad7
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-02 13:53:17 +0900

    refector: 사과나무 grid 만드는 부분 수정

--- 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/7. 사과나무/AA.py
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/7. 사과나무/AA.py
@@ -8,14 +8,14 @@ if sys.argv[-1] == "local":
     sys.stdin = open(filePath, "rt")
 
 gridLength = int(input())
-grid = list(map(list, [input().split() for _ in range(gridLength)]))
+grid = [list(map(int, input().split())) for _ in range(gridLength)]
 half = gridLength // 2
 result = 0
 
 for i in range(gridLength):
     if i <= half:
-        result += reduce(lambda total, x: total + int(x), grid[i][half-i: half+i+1], 0)
+        result += reduce(lambda total, x: total + x, grid[i][half-i: half+i+1], 0)
     else:
-        result += reduce(lambda total, x: total + int(x), grid[i][i-half: gridLength-(i-half)], 0)
+        result += reduce(lambda total, x: total + x, grid[i][i-half: gridLength-(i-half)], 0)
 
 print(result)
