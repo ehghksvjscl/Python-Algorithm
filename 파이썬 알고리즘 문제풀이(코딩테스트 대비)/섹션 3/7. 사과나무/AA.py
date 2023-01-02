9d4781e96bfd28116e1bc675b924267f66bd452c
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-02 10:54:38 +0900

    사과 나무.. 시간이 오래 걸렸다.

---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/7. 사과나무/AA.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from functools import reduce
+
+filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.argv[-1] == "local":
+    sys.stdin = open(filePath, "rt")
+
+gridLength = int(input())
+grid = list(map(list, [input().split() for _ in range(gridLength)]))
+half = gridLength // 2
+result = 0
+
+for i in range(gridLength):
+    if i <= half:
+        result += reduce(lambda total, x: total + int(x), grid[i][half-i: half+i+1], 0)
+    else:
+        result += reduce(lambda total, x: total + int(x), grid[i][i-half: gridLength-(i-half)], 0)
+
+print(result)
