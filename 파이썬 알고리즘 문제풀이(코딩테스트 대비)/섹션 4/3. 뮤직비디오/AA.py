9dcaf5331dfe1e0bb032182ba34d0e165b952e14
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-12 16:34:57 +0900

    뮤직비디오 문제(결정알고리즘) 이분탐색

---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 4/3. 뮤직비디오/AA.py
@@ -0,0 +1,35 @@
+import sys
+import os
+from functools import reduce
+
+filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.argv[-1] == "local":
+    sys.stdin = open(filePath, "rt")
+
+singCount, hasDVD = map(int, input().split())
+recordTimeList = list(map(int, input().split()))
+
+lt = 1
+rt = sum(recordTimeList)
+res = 0
+
+while lt<=rt:
+    mid = (rt + lt) // 2 
+    cnt = 1
+    total = 0
+    for i in recordTimeList:
+        if total + i > mid:
+            cnt += 1
+            total = 0
+        
+        total += i
+
+
+    if cnt <= hasDVD:
+        rt = mid - 1
+        res = mid
+    else:
+        lt = mid + 1
+
+print(res)
