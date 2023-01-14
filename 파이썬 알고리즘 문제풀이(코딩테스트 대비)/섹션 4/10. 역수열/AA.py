0b82ebaa522a16a48a4f420cda03d4419a3c4d64
ehghksvjscl <ehghkswodnjs@naver.com>
2023-01-14 19:15:43 +0900

    역수열

---
+++ 파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 4/10. 역수열/AA.py
@@ -0,0 +1,23 @@
+import sys
+import os
+from functools import reduce
+
+filePath = "{}/in1.txt".format(os.path.dirname(os.path.abspath(__file__)))
+
+if sys.argv[-1] == "local":
+    sys.stdin = open(filePath, "rt")
+
+n = int(input())
+array = list(map(int, input().split()))
+
+tmp = [0] * n
+
+for i in range(n):
+    for j in range(n):
+        if array[i] == 0 and tmp[j] == 0:
+            tmp[j] = i + 1
+            break
+        elif tmp[j] == 0:
+            array[i] -= 1
+
+print(*tmp)
